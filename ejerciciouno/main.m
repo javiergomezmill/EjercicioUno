d26e104c7acccb51bda3b745a975a17e6b419a95
CCDM11 <LABORATORIOMAC@iMac-de-CCDM11.local>
2014-08-15 10:09:04 -0600

    Initial Commit

---
+++ ejerciciouno/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ejerciciouno
+//
+//  Created by CCDM11 on 15/08/14.
+//  Copyright (c) 2014 CCDM11. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
